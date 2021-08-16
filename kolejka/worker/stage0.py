255f507f50dd600a10ebcf59773420d74392c054
Rafal Jankowski <raalsky@gmail.com>
2021-08-16 23:45:36 +0200

    Refactor

--- kolejka/worker/stage0.py
+++ kolejka/worker/stage0.py
@@ -63,7 +63,7 @@ def stage0(task_path, result_path, temp_path=None, consume_task_folder=False):
 
     docker_task = 'kolejka_worker_{}'.format(task.id)
 
-    docker_before_run = []
+    before_run = []
 
     docker_cleanup  = [
         [ 'docker', 'kill', docker_task ],
@@ -137,14 +137,14 @@ def stage0(task_path, result_path, temp_path=None, consume_task_folder=False):
             docker_call += [ '--runtime=nvidia', '--shm-size=1g', '--gpus', f'\'"device={gpus_str}"\'' ]
 
             if task.limits.gpu_memory is not None and task.limits.gpu_memory > 0:
-                docker_gpu_memory_reservation = ['docker', 'run', '--runtime=nvidia', '--rm', '-d', '-e',
+                gpu_memory_reservation = ['docker', 'run', '--runtime=nvidia', '--rm', '-d', '-e',
                                                  'NVIDIA_VISIBLE_DEVICES=0', '--name',
                                                  f'gpu_mem_preserve_{task.id}', 'gpu-memory-reservation:latest',
                                                  'python3', '/app/reserve_gpu_memory.py',
                                                  f'{task.limits.gpu_memory // 1024 // 1024}']
-                logging.debug('Docker call : {}'.format(docker_gpu_memory_reservation))
-                docker_before_run += [
-                    docker_gpu_memory_reservation
+                logging.debug('Docker call : {}'.format(gpu_memory_reservation))
+                before_run += [
+                    gpu_memory_reservation
                 ]
                 docker_cleanup += [
                     ['docker', 'stop', f'gpu_mem_preserve_{task.id}'],
@@ -210,8 +210,8 @@ def stage0(task_path, result_path, temp_path=None, consume_task_folder=False):
         result.stdout = task.stdout
         result.stderr = task.stderr
 
-        for docker_before in docker_before_run:
-            silent_call(docker_before)
+        for sub_call in before_run:
+            silent_call(sub_call)
 
         start_time = datetime.datetime.now()
         docker_run = subprocess.run(docker_call, stdout=subprocess.PIPE)
